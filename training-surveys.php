8266d6f2d3d8ebaaf3e0fe1ea090acd908d39fd7
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2016-03-26 08:26:35 -0400

    Add preliminary entrance survey page

--- training-surveys.php
+++ training-surveys.php
@@ -39,6 +39,26 @@ function wordpressdc_add_entrance_survey_dashboard_widget() {
 add_action( 'wp_dashboard_setup', 'wordpressdc_add_entrance_survey_dashboard_widget' );
 
 function wordpressdc_render_entrance_survey_dashboard_widget() {
-	echo "Welcome! Take the <a href='/'>Entrance Survey!</a>";
+	echo "<h2>Welcome!</h2>";
+	echo "<p>Help WordPressDC provide the best training possible by taking the entrance survey.</p>";
+	echo "<a class='button button-primary' href='" . admin_url( 'index.php?page=wordpressdc-entrance-survey' ) . "'>Start Survey</a>";
 }
 
+
+// Add entrance survey page
+function wordpressdc_entrance_survey_menu() {
+	add_dashboard_page( 'WordPressDC Training Entrance Survey', 'Entrance Survey', 'manage_options', 'wordpressdc-entrance-survey', 'wordpressdc_render_entrance_survey' );
+}
+
+
+add_action( 'admin_menu', 'wordpressdc_entrance_survey_menu' );
+
+function wordpressdc_render_entrance_survey() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<h2>Entrance Survey</h2>';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
